e72f642b641c2671078926ceb0f91241cf511215
yayatriplesix <s7070135@kmitl.ac.th>
2014-11-20 23:48:42 +0700

    GUI test
    Add Tkinter

--- Project.py
+++ Project.py
@@ -12,23 +12,49 @@ Author : Yaya & Tonpho @ITKMITL
 Last Modified Date : 20/11/2014 Time : 22:19
 Language : Python 2.7.8
 
-eiei jubjub
 '''
-def question(num, quest):
-    for i in xrange(num):
-        quest_name = raw_input('Please Enter Your Question :\n')
-        if quest_name == '_reset':
-            reset()
-        quest[quest_name] = 0
-        choice = choice_func(input('Please Enter The Number of Your Choices :\n'), [])
-        correct = input('Please Enter Your Correct Choice :\n')
-        quest[quest_name] += correct
+import Tkinter as tk
+root = tk.Tk()
+root.geometry('500x200')
+root.title('The-Examer')
+tk.Label(root, text='Please Enter The Number of Your Question :').pack()
+num = tk.IntVar()
+entry = tk.Entry(root, textvariable=num)
+entry.pack()
 
-def choice_func(num, choice):
+
+
+def question():
+    root = tk.Tk()
+    root.geometry('500x200')
+    root.title('The-Examer')
+    quest = {}
+    global num
+    num = num.get()
     for i in xrange(1, num+1):
-        choice.append(raw_input('Enter Your Choice ' + '(' + str(i) + ') :\n'))
-    return choice
-def reset():
-    question(input('Please Enter The Number of Your Question :\n'), {})
+        quest_name = tk.StringVar()
+        tk.Label(root, text='Please Enter Your Question %d :' % i).pack()
+        entry = tk.Entry(root, textvariable=quest_name)
+        entry.pack()
+    tk.Button(root, text='Submit').pack()
+##        if quest_name == '_reset':
+##            reset()
+##        quest[quest_name.get()] = 0
+##        choice = choice_func(input('Please Enter The Number of Your Choices :\n'), [])
+##        correct = input('Please Enter Your Correct Choice :\n')
+##        quest[quest_name.get()] += correct
+##        print quest
+##        print choice
+        
+##def choice_func(num, choice):
+##    for i in xrange(1, num+1):
+##        choice.append(raw_input('Enter Your Choice '+'('+str(i)+') :\n'))
+##    return choice
 
-question(input('Please Enter The Number of Your Question :\n'), {})
+##def reset():
+##    question(input('Please Enter The Number of Your Question :\n'), {})
+
+tk.Button(root, text='Create', command=question).pack()
+##
+##question(input('Please Enter The Number of Your Question :\n'), {})
+root.mainloop()
